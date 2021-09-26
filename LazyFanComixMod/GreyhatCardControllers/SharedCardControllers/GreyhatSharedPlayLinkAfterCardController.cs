0662f828be42acaf3d44220a6b536a19766fcc5d
Ruduen <45407351+Ruduen@users.noreply.github.com>
2021-09-26 14:34:52 -0500

    Revert Small Changes from In Progress Revamp Work

---
+++ LazyFanComixMod/GreyhatCardControllers/SharedCardControllers/GreyhatSharedPlayLinkAfterCardController.cs
@@ -0,0 +1,38 @@
+﻿using Handelabra.Sentinels.Engine.Controller;
+using Handelabra.Sentinels.Engine.Model;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+// Manually tested!
+
+namespace LazyFanComix.Greyhat
+{
+    public abstract class GreyhatSharedPlayLinkFirstCardController : GreyhatSharedCheckNextToLinkCardController
+    {
+        public GreyhatSharedPlayLinkFirstCardController(Card card, TurnTakerController turnTakerController) : base(card, turnTakerController)
+        {
+        }
+
+        public override IEnumerator Play()
+        {
+            IEnumerator coroutine;
+            List<PlayCardAction> storedResults = new List<PlayCardAction>();
+
+            coroutine = PostLinkPlay();
+            if (this.UseUnityCoroutines) { yield return this.GameController.StartCoroutine(coroutine); } else { this.GameController.ExhaustCoroutine(coroutine); }
+
+            // Play
+            coroutine = this.GameController.SelectAndPlayCardsFromHand(this.DecisionMaker, 1, false, 0, new LinqCardCriteria((Card c) => c.IsLink, "link"), storedResults: storedResults, cardSource: this.GetCardSource());
+            if (this.UseUnityCoroutines) { yield return this.GameController.StartCoroutine(coroutine); } else { this.GameController.ExhaustCoroutine(coroutine); }
+
+            if (!(storedResults.Count > 0 && storedResults.FirstOrDefault().WasCardPlayed))
+            {
+                coroutine = this.GameController.DrawCards(this.HeroTurnTakerController, 2, cardSource: this.GetCardSource());
+                if (this.UseUnityCoroutines) { yield return this.GameController.StartCoroutine(coroutine); } else { this.GameController.ExhaustCoroutine(coroutine); }
+            }
+        }
+
+        protected abstract IEnumerator PostLinkPlay();
+    }
+}
