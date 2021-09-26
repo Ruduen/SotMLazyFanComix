41067f5634a7504d14a1f08cccb1b3502dddb6d9
Ruduen <45407351+Ruduen@users.noreply.github.com>
2021-09-26 14:44:03 -0500

    Revert changes from partial revamp.

--- LazyFanComixMod/GreyhatCardControllers/SharedCardControllers/GreyhatSharedPlayLinkAfterCardController.cs
+++ LazyFanComixMod/GreyhatCardControllers/SharedCardControllers/GreyhatSharedPlayLinkAfterCardController.cs
@@ -19,18 +19,13 @@ namespace LazyFanComix.Greyhat
             IEnumerator coroutine;
             List<PlayCardAction> storedResults = new List<PlayCardAction>();
 
-            coroutine = PostLinkPlay();
-            if (this.UseUnityCoroutines) { yield return this.GameController.StartCoroutine(coroutine); } else { this.GameController.ExhaustCoroutine(coroutine); }
-
             // Play
             coroutine = this.GameController.SelectAndPlayCardsFromHand(this.DecisionMaker, 1, false, 0, new LinqCardCriteria((Card c) => c.IsLink, "link"), storedResults: storedResults, cardSource: this.GetCardSource());
             if (this.UseUnityCoroutines) { yield return this.GameController.StartCoroutine(coroutine); } else { this.GameController.ExhaustCoroutine(coroutine); }
 
-            if (!(storedResults.Count > 0 && storedResults.FirstOrDefault().WasCardPlayed))
-            {
-                coroutine = this.GameController.DrawCards(this.HeroTurnTakerController, 2, cardSource: this.GetCardSource());
-                if (this.UseUnityCoroutines) { yield return this.GameController.StartCoroutine(coroutine); } else { this.GameController.ExhaustCoroutine(coroutine); }
-            }
+            coroutine = PostLinkPlay();
+            if (this.UseUnityCoroutines) { yield return this.GameController.StartCoroutine(coroutine); } else { this.GameController.ExhaustCoroutine(coroutine); }
+
         }
 
         protected abstract IEnumerator PostLinkPlay();
